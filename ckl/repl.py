cae468af65821804e15587d105479f0cc2bf92f8
Damian Brunold <dab@dabsoft.ch>
2022-04-05 12:30:36 +0200

    Handle Ctrl-D to terminate REPL

--- ckl/repl.py
+++ ckl/repl.py
@@ -9,34 +9,37 @@ def main():
     interpreter = ckl.interpreter.Interpreter(False, False)
     # TODO module path
     # TODO process files
-    line = input("> ")
-    while line != "exit":
-        try: 
-            ckl.parser.parse_script(line, "{stdin}")
-        except CklSyntaxError as e:
-            if e.msg.startswith("Unexpected end of input"):
+    try:
+        line = input("> ")
+        while line != "exit":
+            try: 
+                ckl.parser.parse_script(line, "{stdin}")
+            except CklSyntaxError as e:
+                if e.msg.startswith("Unexpected end of input"):
+                    line += input("+ ")
+                    continue
+            except Exception:
                 line += input("+ ")
                 continue
-        except Exception:
-            line += input("+ ")
-            continue
 
-        if not line == ";":
-            try:
-                value = interpreter.interpret(line, "{stdin}")
-                if value.isReturn():
-                    value = value.asReturn().value
-                if value != NULL:
-                    print(value)
-            except CklRuntimeError as e:
-                print(str(e.value.asString().value) + ": " + e.msg + " (Line " + str(e.pos) + ")")
-                if e.stacktrace:
-                    for st in e.stacktrace:
-                        print(str(st))
-            except CklSyntaxError as e:
-                print(e.msg + ((" (Line " + str(e.pos) + ")") if e.pos else ""))
+            if not line == ";":
+                try:
+                    value = interpreter.interpret(line, "{stdin}")
+                    if value.isReturn():
+                        value = value.asReturn().value
+                    if value != NULL:
+                        print(value)
+                except CklRuntimeError as e:
+                    print(str(e.value.asString().value) + ": " + e.msg + " (Line " + str(e.pos) + ")")
+                    if e.stacktrace:
+                        for st in e.stacktrace:
+                            print(str(st))
+                except CklSyntaxError as e:
+                    print(e.msg + ((" (Line " + str(e.pos) + ")") if e.pos else ""))
 
-        line = input("> ")
+            line = input("> ")
+    except EOFError:
+        pass
 
 if __name__ == "__main__":
     main()
