43aad50a309d634bfe033f69db740e9e676a95ad
Damian Brunold <damian.brunold@bluewin.ch>
2023-02-22 17:30:39 +0100

    Switch to declarative setup.cfg, add modules to wheel, bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-        name="ckl", 
-        version="3.5.2",
-        author="Damian Brunold",
-        author_email="dab@dabsoft.ch",
-        packages=find_packages(),
-        package_data={"checkerlang-py": ["modules/*.ckl"]},
-)
+from setuptools import setup
+setup()
